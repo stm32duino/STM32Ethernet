ea18d121f02db38899a01aec32a9c87944ded073
Alexandre Bourdiol <alexandre.bourdiol@st.com>
2020-12-03 09:46:17 +0100

    Run astyle
    Fix astyle issue
    
    Signed-off-by: Alexandre Bourdiol <alexandre.bourdiol@st.com>

--- src/lwipopts.h
+++ src/lwipopts.h
@@ -9,12 +9,12 @@
 
 /* LwIP specific configuration options. */
 #if __has_include("STM32lwipopts.h")
-#include "STM32lwipopts.h"
+  #include "STM32lwipopts.h"
 #else
-#if __has_include("lwipopts_extra.h")
-#include "lwipopts_extra.h"
-#endif
-#include "lwipopts_default.h"
+  #if __has_include("lwipopts_extra.h")
+    #include "lwipopts_extra.h"
+  #endif
+  #include "lwipopts_default.h"
 #endif
 
 #endif /* _ARDUINO_LWIPOPTS_H */
--- src/utility/stm32_eth.h
+++ src/utility/stm32_eth.h
@@ -112,7 +112,7 @@ struct tcp_struct {
 #define MAX_CLIENT  32
 
 #ifdef ETH_INPUT_USE_IT
-extern struct netif gnetif;
+  extern struct netif gnetif;
 #endif
 
 
@@ -125,22 +125,22 @@ void stm32_eth_scheduler(void);
 void User_notification(struct netif *netif);
 
 #if LWIP_DHCP
-void stm32_DHCP_Process(struct netif *netif);
-void stm32_DHCP_Periodic_Handle(struct netif *netif);
-void stm32_DHCP_manual_config(void);
-uint8_t stm32_get_DHCP_lease_state(void);
-void stm32_set_DHCP_state(uint8_t state);
-uint8_t stm32_get_DHCP_state(void);
-uint8_t stm32_dhcp_started(void);
+  void stm32_DHCP_Process(struct netif *netif);
+  void stm32_DHCP_Periodic_Handle(struct netif *netif);
+  void stm32_DHCP_manual_config(void);
+  uint8_t stm32_get_DHCP_lease_state(void);
+  void stm32_set_DHCP_state(uint8_t state);
+  uint8_t stm32_get_DHCP_state(void);
+  uint8_t stm32_dhcp_started(void);
 #else
-#error "LWIP_DHCP must be enabled in lwipopts.h"
+  #error "LWIP_DHCP must be enabled in lwipopts.h"
 #endif
 
 #if LWIP_DNS
-void stm32_dns_init(const uint8_t *dnsaddr);
-int8_t stm32_dns_gethostbyname(const char *hostname, uint32_t *ipaddr);
+  void stm32_dns_init(const uint8_t *dnsaddr);
+  int8_t stm32_dns_gethostbyname(const char *hostname, uint32_t *ipaddr);
 #else
-#error "LWIP_DNS must be enabled in lwipopts.h"
+  #error "LWIP_DNS must be enabled in lwipopts.h"
 #endif
 
 #if LWIP_UDP
@@ -164,11 +164,11 @@ ip_addr_t *u8_to_ip_addr(uint8_t *ipu8, ip_addr_t *ipaddr);
 uint32_t ip_addr_to_u32(ip_addr_t *ipaddr);
 
 #if LWIP_TCP
-err_t tcp_connected_callback(void *arg, struct tcp_pcb *tpcb, err_t err);
-err_t tcp_accept_callback(void *arg, struct tcp_pcb *newpcb, err_t err);
-void tcp_connection_close(struct tcp_pcb *tpcb, struct tcp_struct *tcp);
+  err_t tcp_connected_callback(void *arg, struct tcp_pcb *tpcb, err_t err);
+  err_t tcp_accept_callback(void *arg, struct tcp_pcb *newpcb, err_t err);
+  void tcp_connection_close(struct tcp_pcb *tpcb, struct tcp_struct *tcp);
 #else
-#error "LWIP_TCP must be enabled in lwipopts.h"
+  #error "LWIP_TCP must be enabled in lwipopts.h"
 #endif
 
 #endif /* __STM32_ETH_H__ */
